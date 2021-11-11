71dc3fe6433b71bc66ef7b826335cdfd793aff2a
MaksymM-cultureH <maksym.malynovskyi@culturehint.com>
2021-11-11 13:56:50 +0000

    authentication done

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,11 @@ import Signup from "./Signup"
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "../context/AuthContext"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-// import Dashboard from "./Dashboard"
-// import Login from "./Login"
-// import PrivateRoute from "./PrivateRoute"
-// import ForgotPassword from "./ForgotPassword"
-// import UpdateProfile from "./UpdateProfile"
+import Dashboard from "./Dashboard"
+import Login from "./Login"
+import PrivateRoute from "./PrivateRoute"
+import ForgotPassword from "./ForgotPassword"
+import UpdateProfile from "./UpdateProfile"
 
 function App() {
   return (
@@ -19,11 +19,12 @@ function App() {
         <Router>
           <AuthProvider>
             <Switch>
-              <Route exact path="/" component={Signup} />
-              {/* <PrivateRoute path="/update-profile" component={UpdateProfile} /> */}
+              <PrivateRoute exact path="/" component={Dashboard}/>
+              <Route path="/signup" component={Signup} />
+              <PrivateRoute path="/update-profile" component={UpdateProfile} />
               {/* <Route path="/signup" component={Signup} /> */}
-              {/* <Route path="/login" component={Login} /> */}
-              {/* <Route path="/forgot-password" component={ForgotPassword} /> */}
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
             </Switch>
           </AuthProvider>
         </Router>
